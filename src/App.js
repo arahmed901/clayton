56e2f9b0dfc1be5acf0207724db1a9b564480ad3
Abdurrahman Ahmed <abdahmed@WKMZTAFA5131.global.publicisgroupe.net>
2023-01-26 11:22:19 -0500

    testing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from './Home/Home';
-import GlobalStyle from './base';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home/Home";
+import GlobalStyle from "./base";
 
 const App = () => {
- 
   return (
-      <div className="site-container">
-          <GlobalStyle />
-          <Switch>
-            <Route exact path="/" component={Home} />
-          </Switch>
-      </div>
-  )
+    <div className="site-container">
+      <GlobalStyle />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        this is a test
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
